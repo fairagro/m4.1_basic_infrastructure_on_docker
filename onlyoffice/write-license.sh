917d9c33c0a8c705bf7460114cf541b9af6e96cf
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2025-06-19 16:04:15 +0200

    introduced onlyoffice

---
+++ onlyoffice/write-license.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+set -eu
+
+LICENSE_FILE="/var/www/onlyoffice/Data/license.lic"
+if [ -n "$ONLYOFFICE_LICENSE" ]; then
+    echo "$ONLYOFFICE_LICENSE" > "$LICENSE_FILE";
+    chown ds:ds "$LICENSE_FILE";
+    chmod 400 "$LICENSE_FILE";
+fi;
+
+exec /app/ds/run-document-server.sh "$@"
